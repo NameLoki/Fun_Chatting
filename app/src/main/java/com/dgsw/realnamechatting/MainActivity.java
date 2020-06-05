93c6083f41e34cd0e7bf663ad75bebb1c3e46a26
NameLoki <3334635@naver.com>
2020-06-05 09:26:56 +0900

    FileUpload

---
+++ app/src/main/java/com/dgsw/realnamechatting/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dgsw.realnamechatting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dgsw.realnamechatting.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+    }
+}
